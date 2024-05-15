bcfcefc7203cbfd78a2143f94c771c00af754869
JangHyeonJun2 <epfvkdlxj@naver.com>
2024-05-15 23:57:10 +0900

    인프런 알고리즘 문제풀이, 단어 뒤집기 Question

---
+++ src/inflearn/단어뒤집기.java
@@ -0,0 +1,16 @@
+package inflearn;
+
+/*
+ * 설명: N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요.
+ * 입력: 첫 줄에 자연수 N(3<=N<=20)이 주어집니다.
+ * 출력: N개의 단어를 입력된 순서대로 한 줄에 하나씩 뒤집어서 출력합니다.
+ */
+
+public class 단어뒤집기 {
+    public void solution(int number) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
