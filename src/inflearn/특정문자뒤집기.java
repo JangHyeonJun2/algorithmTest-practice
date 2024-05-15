ff04a5717212b985a2f49b5478d3b767b629d1fd
JangHyeonJun2 <epfvkdlxj@naver.com>
2024-05-16 00:23:10 +0900

    인프런 알고리즘 문제풀이, [특정 문자 뒤집기] Question

---
+++ src/inflearn/특정문자뒤집기.java
@@ -0,0 +1,14 @@
+package inflearn;
+/*
+ * 설명: 영어 알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고, 특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램을 작성하세요.
+ * 입력: 첫 줄에 길이가 100을 넘지 않는 문자열이 주어진다.
+ * 출력: 첫 줄에 알파벳만 뒤집힌 문자열을 출력한다.
+ */
+public class 특정문자뒤집기 {
+    public static void solution(String str) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
