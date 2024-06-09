f7596029ca9eb7567f190f22ddd53fedede2dbe1
JangHyeonJun2 <epfvkdlxj@naver.com>
2024-06-09 23:54:14 +0900

    인프런 알고리즘 문제풀이, [중복문자제거] Que

---
+++ src/inflearn/회전문자열.java
@@ -0,0 +1,19 @@
+package inflearn;
+
+import java.util.Scanner;
+
+/*
+ * 설명: 앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 한다.
+ *      문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES", 회문 문자열이 아니면 "NO"를 출력하는 프로그램을 작성하여라
+ *      단, 회문을 검사할 때 대소문자를 구분하지 않는다.
+ * 입력: 첫 줄에 길이 100을 넘지 않는 공백이 없는 문자열이 주어진다.
+ * 출력: 첫 번쨰 줄에 회문 문자열인지의 결과를 YES 또는 NO로 출력한다.
+ */
+public class 회전문자열 {
+    public static void solution(String str) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
