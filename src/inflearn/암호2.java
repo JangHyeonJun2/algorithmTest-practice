e230093ea264702735535a5afcdbe2c063290d8e
JangHyeonJun2 <epfvkdlxj@naver.com>
2024-07-31 01:32:38 +0900

    인프런 알고리즘 문제풀이, [암호2] Sol

---
+++ src/inflearn/암호2.java
@@ -0,0 +1,20 @@
+package inflearn;
+
+import java.util.Scanner;
+
+public class 암호2 {
+    public static void solution(int num, String str) {
+        StringBuilder answer = new StringBuilder();
+        for (int i=0; i<num; i++) {
+            answer.append((char)Integer.parseInt(str.substring(0, 7).replace('#', '1').replace('*', '0'), 2));
+            str = str.substring(7);
+        }
+
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        solution(sc.nextInt(), sc.next());
+    }
+}
