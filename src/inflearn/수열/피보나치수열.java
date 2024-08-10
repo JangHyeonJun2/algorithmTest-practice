2cbe924f0f5352c67fb6653e51f65cab1ccb492f
JangHyeonJun2 <epfvkdlxj@naver.com>
2024-08-11 00:51:54 +0900

    인프런 알고리즘 문제풀이, [피보나치수열] Sol

---
+++ src/inflearn/수열/피보나치수열.java
@@ -0,0 +1,31 @@
+package inflearn.수열;
+
+import java.util.Scanner;
+
+public class 피보나치수열 {
+    public int[] solution(int num) {
+        int[] answer = new int[num];
+        int first = 1;
+        int second = 1;
+
+        answer[0] = first;
+        answer[1] = second;
+
+        for (int i=2; i<num; i++) {
+            answer[i] = first + second;
+            first = second;
+            second = answer[i];
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        피보나치수열 main = new 피보나치수열();
+        int[] solution = main.solution(sc.nextInt());
+        for (int n : solution) {
+            System.out.print(n);
+            System.out.print(" ");
+        }
+    }
+}
