443d89a907c9c1524e75aeecac336221f6c5aea2
janghyeonjun2 <epfvkdlxj@naver.com>
2024-12-08 23:10:55 +0900

    뼈구이 문제풀이, 게임뱁 최단거리 Not Sol

---
+++ src/main/java/bornroast/GameMap.java
@@ -0,0 +1,11 @@
+package main.java.bornroast;
+
+/*
+ * 문제 URL: https://school.programmers.co.kr/learn/courses/30/lessons/1844
+ */
+public class GameMap {
+    public int solution(int[][] maps) {
+        int answer = 0;
+        return answer;
+    }
+}
